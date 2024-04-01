bf9f488d28de2718ff6f12fef005f7cbd6c5a77f
Nazrul Islam Sajib <124718648+NazrulIslamSajib@users.noreply.github.com>
2024-04-02 00:39:01 +0600

    Create class2.js

---
+++ class2.js
@@ -0,0 +1,5 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     let id1 = document.getElementById("id1");
+//     id1.innerHTML = "sajisfasfb";
+// });
+document.getElementsByClassName("classname1");
