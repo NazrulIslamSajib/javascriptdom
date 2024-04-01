82ded48e58abfd23350c2bfc8b45e6d41bafee45
Nazrul Islam Sajib <124718648+NazrulIslamSajib@users.noreply.github.com>
2024-04-02 02:16:22 +0600

    Create dombasicjs.js

---
+++ dombasicjs.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let element = document.getElementsByClassName("classname1")[1];
+    element.innerHTML = "Subscribe my channel"; 
+    let heading5=document.getElementsByName("heading5")[0]; 
+    heading5.innerHTML="Change your code"; 
+    let changealltag=document.getElementsByTagName("h1")[0];
+    changealltag.innerHTML="All sajib kore dao";
+ });
+// let element = document.getElementsByClassName("classname1")[0];
+// element.innerHTML = "Subscribe my channel";
+/*
+
+    document.addEventListener("DOMContentLoaded", function() {
+    let element = document.getElementsByClassName("classname1")[1];
+    element.innerHTML = "Subscribe my channel"; 
+    let heading5 = document.getElementsByName("heading5")[0]; // Corrected typo
+    heading5.innerHTML = "Change your code"; // Corrected typo
+});
+
+
+*/
